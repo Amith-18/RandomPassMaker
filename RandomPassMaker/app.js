40fb8bb53bdb15f712d843ec607b06e844a94a39
Amith-18 <155166599+Amith-18@users.noreply.github.com>
2024-11-13 13:21:51 +0400

    Add files via upload

---
+++ RandomPassMaker/app.js
@@ -0,0 +1,36 @@
+
+
+const passwordBox   =   document.getElementById('password');
+const length    =   12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number    =   "0123456789";
+const symbol    =   "~!@#$%^&*()_+?><:}{,./;''[]-=";
+
+const allchars  =   upperCase + lowerCase + number + symbol;
+
+
+function crearePassword(){
+    let password    =  "";
+    password    += upperCase[Math.floor(Math.random()*upperCase.length)];
+    password    += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password    += number[Math.floor(Math.random()*number.length)];
+    password    += symbol[Math.floor(Math.random()*symbol.length)];
+
+
+    while(length    > password.length){
+        password += allchars[Math.floor(Math.random()*allchars.length)];
+    }
+
+    passwordBox.value = password;
+    console.log(password);
+}
+
+
+function copyPaste(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+
